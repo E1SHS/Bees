69fc79e2e411b3c27871883064757ae624456836
E1SHS <einars.susts@inbox.lv>
2024-04-17 14:57:39 +0300

    back

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 
 <template>
     <SignupForm />
-    <MenuForm />
-    <DeadHiveForm />
-    <MiteForm />
-    <RequeenForm />
-    <FeedBeesForm />
-    <DocumentFloraForm />
-    <HarvestHoneyForm />
-    <WinterizeForm />
-    <TreatmentForm />
    <router-view />
 </template>
 
 <script setup lang="ts">
 
 import SignupForm from './components/SignupForm.vue';
-import MenuForm from './components/MenuForm.vue';
-import DeadHiveForm from './components/DeadHiveForm.vue';
-import MiteForm from './components/MiteForm.vue';
-import RequeenForm from './components/RequeenForm.vue';
-import FeedBeesForm from './components/FeedBeesForm.vue';
-import DocumentFloraForm from './components/DocumentFloraForm.vue';
-import HarvestHoneyForm from './components/HarvestHoneyForm.vue';
-import WinterizeForm from './components/WinterizeForm.vue';
-import TreatmentForm from './components/TreatmentForm.vue';
 
 </script>
 <style lang="scss" scoped>
